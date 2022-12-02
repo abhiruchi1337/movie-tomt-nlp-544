46597671e0afeced0926caccc50839e0ee2c247f
Abhiruchi Bhattacharya <40300910+abhiruchi1337@users.noreply.github.com>
2022-12-01 21:09:00 -0800

    roberta classifier

--- bert_bertscore/test.py
+++
@@ -1,9 +0,0 @@
-import os
-import json
-
-os.chdir('..')
-DATA_PATH=os.getcwd()+'/data/'
-
-with open(DATA_PATH+'sentenceWikiTopGrossing.txt', "r") as wiki:
-  dataset = json.loads(wiki.read())
-  print(dataset)
